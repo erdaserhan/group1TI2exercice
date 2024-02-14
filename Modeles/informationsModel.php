099e96cd998301828bd9790a2ba93a6803c64e5e
mikhawa <michaeljpitz@gmail.com>
2024-02-14 07:24:07 +0100

    first commit

---
+++ Modeles/informationsModel.php
@@ -0,0 +1,12 @@
+<?php
+// Notre fonction qui charge les informations
+function getInformations(PDO $db)
+{
+
+}
+
+// notre fonction qui insert dans informations
+function addInformations(PDO $db)
+{
+    
+}
---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Chargement des dépendances
+
+// Connexion à la base de donnée
+
+// Si le formulaire a été envoyé
+
+    // On insert dans la table `informations` si valide
+
+// on récupère toutes les entrées de la table
+// `informations`
+
+// on charge le template qui affiche la vue
+
+// on ferme la connexion 
